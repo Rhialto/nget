720177daed11ac68fe18eae9d6ab0c235a82c01b
Olaf Seibert <rhialto@falu.nl>
2018-04-21 19:15:41 +0200

    Rename configure.in to configure.ac, and add aclocal.

--- autogen.sh
+++ autogen.sh
@@ -3,8 +3,10 @@
 # Does everything needed to get a fresh cvs checkout to compilable state.
 
 set -e
+echo "running aclocal"
+aclocal --force
 echo "running autoconf"
-autoconf
+autoconf --force
 echo "running autoheader"
 autoheader
 echo "running ./configure --enable-maintainer-mode" "$@"
